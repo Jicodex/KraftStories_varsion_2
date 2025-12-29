77dcc942ed884f3019cd4f805dedd6264ceac607
jicodex <enexcode@gmail.com>
2025-12-29 21:31:54 +0600

    zoom

--- assets/js/script.js
+++ assets/js/script.js
@@ -542,3 +542,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // ======== about page mision vision silder 
 
+document.querySelectorAll('.single_product_view').forEach(box => {
+            const img = box.querySelector('img');
+
+            box.addEventListener('mousemove', (e) => {
+                const rect = box.getBoundingClientRect();
+
+                const x = ((e.clientX - rect.left) / rect.width) * 100;
+                const y = ((e.clientY - rect.top) / rect.height) * 100;
+
+                img.style.transformOrigin = `${x}% ${y}%`;
+                img.style.transform = 'scale(1.8)';
+            });
+
+            box.addEventListener('mouseleave', () => {
+                img.style.transformOrigin = 'center center';
+                img.style.transform = 'scale(1)';
+            });
+        });
